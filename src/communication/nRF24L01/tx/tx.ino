4efa04d9b36f36220d0907144964f87f30b0c553
anyongzhi <yongzhi.an@hobot.cc>
2019-01-18 13:49:16 +0800

    update

--- src/communication/nRF24L01/tx/tx.ino
+++ src/communication/nRF24L01/tx/tx.ino
@@ -1,4 +1,3 @@
-<<<<<<< Updated upstream
 /**
  *  Mirf github: https://github.com/E-elektronic/Mirf.git
  */
@@ -11,49 +10,7 @@
 typedef struct _joytick_ctrl_t {
     int rx;
     int ry;
-    bool brake;
-} joytick_ctrl_t;
-
-joytick_ctrl_t ct;
-
-void setup() {
-    Mirf.spi = &MirfHardwareSpi;
-    Mirf.init();
-    Mirf.setRADDR((byte*)"ABCDE"); //设置自己的地址（发送端地址），使用5个字符
-
-    ct.rx = 2;
-    ct.ry = 50;
-    ct.brake = false;
-    Mirf.payload = sizeof(joytick_ctrl_t);
-    Mirf.channel = 90;              //设置所用信道
-    Mirf.config();
-    Serial.begin(9600);
-}
-
-void loop() {
-    Mirf.setTADDR((byte*)"FGHIJ");           //设置接收端地址
-    //value = random(255);                      //0-255的随机数
-    Mirf.send((byte*)&ct);                //发送指令，发送随机数value
-    while(Mirf.isSending()) {
-        delay(1);         //直到发送成功，退出循环
-    }
-    delay(200);
-    Serial.println("Send success, ");
-    //Serial.println(ct);
-}
-=======
-/**
- *  Mirf github: https://github.com/E-elektronic/Mirf.git
- */
-
-#include <SPI.h>
-#include <Mirf.h>
-#include <nRF24L01.h>
-#include <MirfHardwareSpiDriver.h>
-
-typedef struct _joytick_ctrl_t {
-    int rx;
-    int ry;
+    int rz;
     bool brake;
 } joytick_ctrl_t;
 
@@ -75,13 +32,15 @@ void setup() {
 
 int rx = 0;
 int ry = 0;
+int rz = 0;
 int ot = 0;
 
 void loop() {
     Mirf.setTADDR((byte*)"FGHIJ");           //设置接收端地址
     //value = random(255);                      //0-255的随机数
-    ct.rx = 50.0 * cos(rx++ / 20.0) * sin(ry);
+    ct.rx = 50.0 * cos(rx++ / 20.0) * sin(ry) + 0.05 * rx;
     ct.ry = 200.0 * sin(ry++ / 18.0);
+    ct.rz = 100 * sin(sin(rz++/20.0));
     
     Mirf.send((byte*)&ct);                //发送指令，发送随机数value
     while(Mirf.isSending()) {
@@ -91,4 +50,3 @@ void loop() {
     Serial.println("Send success, ");
     //Serial.println(ct);
 }
->>>>>>> Stashed changes
