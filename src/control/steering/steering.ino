9466cbfbd287448b03263a978382e73df760d1b4
sharkka <7572686@qq.com>
2019-01-20 20:51:21 +0800

    debug for steering

--- src/control/steering/steering.ino
+++ src/control/steering/steering.ino
@@ -11,18 +11,24 @@
 Servo myservo;
 
 void setup() {
+    //pinMode(PIN_SERVO, OUTPUT);
     myservo.attach(PIN_SERVO);
+    Serial.begin(9600);
 }
 
 void loop() {
-    myservo.write(0);
-    delay(300);
-    myservo.write(80);
-    delay(300);
-    myservo.write(160);
-    delay(300);
-    myservo.write(80);
-    delay(300);
-    myservo.write(0);
-    delay(300);
+    Serial.println("running...");
+    myservo.write(90);
+    delay(900);
+    for (int i = 90; i <= 180; i++) {
+        myservo.write(i);
+        delay(20);
+    }
+    for (int i = 180; i > 0; i--) {
+        myservo.write(i);
+        delay(20);
+    }
+    delay(1000);
+    
+    //analogWrite(PIN_SERVO, map(800, 0, 1023, 0, 255));
 }
