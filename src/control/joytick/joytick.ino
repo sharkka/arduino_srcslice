7c6ffda67af78aaaa7de9762aa8c140472df87ad
sharkka <47857366@qq.com>
2019-01-21 17:24:52 +0800

    joytick test

---
+++ src/control/joytick/joytick.ino
@@ -0,0 +1,26 @@
+/**
+ * Joytick
+ * test
+ */
+
+int X = A0;
+int Y = A1;
+int BUTTON = 7;
+void setup(void) {
+    Serial.begin(9600);
+    pinMode(BUTTON, INPUT_PULLUP);
+}
+
+void loop(void) {
+    Serial.print("X = ");
+    Serial.print(analogRead(X));
+    Serial.print(", ");
+
+    Serial.print("Y = ");
+    Serial.print(analogRead(Y));
+    Serial.print(", ");
+
+    Serial.print("BUTTON state = ");
+    Serial.println(digitalRead(BUTTON));
+    delay(100);
+}
