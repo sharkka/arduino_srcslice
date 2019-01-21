a1cb8cbb9ccf16139dc0d1ac0c9307bc1556e78f
sharkka <7572686@qq.com>
2019-01-22 00:21:42 +0800

    update

--- src/control/pwm/motor/motor.ino
+++ src/control/pwm/motor/motor.ino
@@ -8,8 +8,13 @@ void setup() {
 }
 
 void loop() {
-
-    for (int i = 500; i < 1000; ++i) {
+    for (int i = 300; i < 1000; ++i) {
+        analogWrite(motorPwm, map(i, 0, 1023, 0, 255));
+        delay(10);
+        Serial.print("pwm puls: ");
+        Serial.println(i);
+    }
+    for (int i = 1000; i < 300; --i) {
         analogWrite(motorPwm, map(i, 0, 1023, 0, 255));
         delay(10);
         Serial.print("pwm puls: ");
