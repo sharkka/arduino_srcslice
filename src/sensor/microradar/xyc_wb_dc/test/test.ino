753ed15180765494e4c0f159f2e3f7028d089787
sharkka <7572686@qq.com>
2019-01-29 07:56:48 +0800

    micro radar control servo motor

--- src/sensor/microradar/xyc_wb_dc/test/test.ino
+++ src/sensor/microradar/xyc_wb_dc/test/test.ino
@@ -1,9 +1,14 @@
 
+#include <Servo.h>
 
 int signalOut = 3;
+int servctrl = 5;
+Servo myservo;
 
-void setup() {
+
+void setup() { 
     pinMode(signalOut, INPUT_PULLUP);
+    myservo.attach(servctrl);
     Serial.begin(9600);
 }
 
@@ -11,4 +16,10 @@ void loop() {
     int ttl = digitalRead(signalOut);
     delay(10);
     Serial.println(ttl);
+    if (HIGH == ttl) {
+        for (int i = 0; i < 90; i ++) {
+            myservo.write(i);
+            delay (50);
+        }
+    }
 }
